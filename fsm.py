bf80600298b43566523ea9d10617e5799c6e710f
ChrisDeantana <christiandeantanastudy@gmail.com>
2022-01-02 16:04:33 +0800

    new commit

--- fsm.py
+++ fsm.py
@@ -1,7 +1,7 @@
 from transitions.extensions import GraphMachine
 
 from linebot.models import MessageTemplateAction
-from utils import send_text_message, send_carousel_message, send_button_message, send_image_message
+from utils import send_text_message, send_button_message, send_image_message
 
 #https://apieceofsushi.com/wp-content/uploads/2020/09/HiraganaChartPinkAPIECEOFSUSHI.COM_-500x707.png - hiragana
 #https://apieceofsushi.com/wp-content/uploads/2020/09/KatakanaChartBlackAPIECEOFSUSHI.COM_.png - katakana

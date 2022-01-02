c981377804069e6a2e50e73c99632bcbb047d97c
ChrisDeantana <christiandeantanastudy@gmail.com>
2022-01-02 12:59:14 +0800

    new commit

--- fsm.py
+++ fsm.py
@@ -13,20 +13,21 @@ class TocMachine(GraphMachine):
         return text.lower() == "choose"
 
     def on_enter_characters(self, event):
-        title = 'Choose what you want to learn'
-        text = 'Choose『Hiragana』Or『Katakana』'
-        btn = [
-            MessageTemplateAction(
-                label='Hiragana',
-                text='Hiragana'
-            ),
-            MessageTemplateAction(
-                label='Katakana',
-                text='Katakana'
-            ),
-        ]
-        url = 'https://i0.wp.com/blog.lingodeer.com/wp-content/uploads/2020/06/%E5%B9%B3%E5%81%87%E7%89%87%E5%81%87%E5%90%8D.png'
-        send_button_message(event.reply_token, title, text, btn, url)
+        # title = 'Choose what you want to learn'
+        # text = 'Choose『Hiragana』Or『Katakana』'
+        # btn = [
+        #     MessageTemplateAction(
+        #         label='Hiragana',
+        #         text='Hiragana'
+        #     ),
+        #     MessageTemplateAction(
+        #         label='Katakana',
+        #         text='Katakana'
+        #     ),
+        # ]
+        # url = 'https://i0.wp.com/blog.lingodeer.com/wp-content/uploads/2020/06/%E5%B9%B3%E5%81%87%E7%89%87%E5%81%87%E5%90%8D.png'
+        # send_button_message(event.reply_token, title, text, btn, url)
+        send_text_message(event.reply_token, 'Choose『Hiragana』Or『Katakana』')
 
     def is_going_to_hiragana(self, event):
         global charHorK
